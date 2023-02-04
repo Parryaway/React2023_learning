e0a4598e683394734f35b47e1ba00725e810eff6
Yoi <106152901+Parryaway@users.noreply.github.com>
2023-02-04 22:16:18 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+function App(){
+    return(
+        <div className="App">
+            <header className="App-header">
+                <p>{2+2}</p>
+            </header>
+        </div>
+
+    )
+}
+export default App;
